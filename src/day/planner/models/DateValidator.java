ffbb379eafd9ce50b00fd3b6a5f4d56df12186a6
ElisavetaTodorova <eli961@abv.bg>
2017-01-08 18:55:24 +0200

    Fixing date validation again

--- src/day/planner/models/DateValidator.java
+++ src/day/planner/models/DateValidator.java
@@ -4,11 +4,11 @@ package day.planner.models;
 public class DateValidator{
 
     public static boolean validate(String input) {
-        String[] hourDayPair = input.split("-");
+        String[] hourDayMount = input.split("-");
 
-        int mount = Integer.parseInt(hourDayPair[0].substring(0, hourDayPair[0].length() - 1));
-        int hour = Integer.parseInt(hourDayPair[1].substring(0, hourDayPair[1].length() - 1));
-        int day = Integer.parseInt(hourDayPair[2].substring(0, hourDayPair[2].length() - 1));
+        int mount = Integer.parseInt(hourDayMount[0].substring(0, hourDayMount[0].length() - 1));
+        int hour = Integer.parseInt(hourDayMount[1].substring(0, hourDayMount[1].length() - 1));
+        int day = Integer.parseInt(hourDayMount[2].substring(0, hourDayMount[2].length() - 1));
 
 
         return !(day > 31 || day < 1) && !(hour > 24 || hour < 1) && !(mount > 12 || mount < 1);
